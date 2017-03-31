a15e9e048f85f4cc67c14efad51ac0fb5556b99b
johuotar <jmh@dnainternet.net>
2017-03-31 14:33:44 +0300

    rewrite

---
+++ NewCsvReaderTest2.py
@@ -0,0 +1,46 @@
+#python 3.6
+import sys as S
+import csv as CS
+import collections as C
+import xml.etree.ElementTree as ET
+import os#dir paths
+
+#script_dir = os.path.dirname(__file__)
+#rel_csv_path = "csvsource.txt"
+#abs_csv_path = os.path.join(script_dir, rel_csv_path)
+
+infile = S.argv[1]
+csvfile = open(infile, "r")
+csvlist = [x.strip() for x in csvfile.readlines()]
+csvfile.close()
+
+AnnoDict = C.defaultdict(list)
+
+for csv in csvlist:
+        print(csv)
+        with open(csv, "r") as file:
+                fileReader = CS.reader(file, delimiter=';', quotechar='|')
+                print("opened " + csv)
+
+                header = next(fileReader)#Python 3 has different kind of next 
+                while not "sentence id" in header:
+                        next(header)#Python 3 has different kind of next 
+
+                IdCol = None
+                AnnoCols = {}
+                for i, col in enumerate(header):
+                        # get id column index
+                        if col.endswith(" id"):
+                                print("ID column found!")
+                                IdCol = i
+                        # get annotation column indices
+                        if col.endswith(" class"):
+                                AnnoCols[i] = col[:-6]
+
+                for row in fileReader:
+                        AnnotationID = row[IdCol] # sentence id
+                        for anno_col in AnnoCols.keys():
+                                if row[anno_col].strip():
+                                     AnnoDict[AnnotationID].append((AnnoCols[anno_col], row[anno_col]))
+                                            
+                print(AnnoDict) #Lets see what the final result is
--- annotationReader.py
+++ annotationReader.py
@@ -1,11 +1,9 @@
+#Python 3.6
 import csv
 import xml.etree.ElementTree as ET
 import os
-import NewCsvReaderTest
-from NewCsvReaderTest import UpperDictionary
-#Python 3.6
-xmlloop = 0#xml loop amount
-
+import NewCsvReaderTest2
+from NewCsvReaderTest2 import AnnoDict
 
 script_dir = os.path.dirname(__file__)
 rel_xml_path = "xmlsource.txt"
@@ -16,36 +14,32 @@ xmllist = xmlfile.read().splitlines()
 AnnoList = []
 KeyList = []
 
-print("lenght of xml list "+ str(len(xmllist)))
-while (xmlloop < len(xmllist)):#stop when all xml files ready
-    tree = ET.parse(xmllist[xmlloop])
-    print(xmllist[xmlloop] + " parsed to tree")
+#while (xmlloop < len(xmllist)):#stop when all xml files ready
+for xml in xmllist:
+    tree = ET.parse(xml)
+    print(xml + " parsed to tree")
     root = tree.getroot()
-    xmlloop = xmlloop + 1
-    print("xmlloop value: " + str(xmlloop))
 
-    for key in UpperDictionary:
+    for key in AnnoDict:
         print("looking for: " + str(key))
         data = root.find(key)#Never finds the key because is looking for solmu type (<Title>)
         #ID number is attribute type, cant find with root.find(x)
-        #print(UpperDictionary[key])#Row by row show the existing annotations
         if data != None:
-            print("SUCCESSFUL FIND!")
-            for k, v in UpperDictionary[key].items():
+            print("Appending to lists")
+            for k, v in AnnoDict[key].items():
                 KeyList.append(k)
                 AnnoList.append(v)
             print(KeyList)
             print(AnnoList)
             print(data)
-        
-            sub1 = ET.SubElement(data, KeyList[0])
-            sub1.text = AnnoList[0]
-            sub2 = ET.SubElement(data, KeyList[1])
-            sub2.text = AnnoList[1]
-            sub3 = ET.SubElement(data, KeyList[2])
-            sub3.text = AnnoList[2]
-        
-        rel_dir_path = r"results\xmlresult{0}.txt".format(xmlloop)
+
+            for a, b in enumerate(AnnoDict):
+                
+                sub = ET.SubElement(data, a)
+                sub.text = b
+                
+                
+        rel_dir_path = r"results\xmlresult{0}.txt".format(xmlloop)#cant use xmlloop var
         abs_dir_path = os.path.join(script_dir, rel_dir_path)
         #.format to make the filenames
                                 
