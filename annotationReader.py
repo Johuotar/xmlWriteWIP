7c5ad0802c14a778dae64668575849f664586c4d
johuotar <jmh@dnainternet.net>
2017-04-04 10:08:41 +0300

    finding id matches

--- annotationReader.py
+++ annotationReader.py
@@ -4,7 +4,8 @@ import xml.etree.ElementTree as ET
 import os
 import NewCsvReaderTest2
 from NewCsvReaderTest2 import AnnoDict
-
+#cd C:\Users\jmfhuo\Desktop\xmlWriteWIP\
+#py annotationReader.py csvsource.txt
 script_dir = os.path.dirname(__file__)
 rel_xml_path = "xmlsource.txt"
 abs_xml_path = os.path.join(script_dir, rel_xml_path)
@@ -15,17 +16,28 @@ xmllist = xmlfile.read().splitlines()
 #AnnoList = []
 #KeyList = []
 
-
 for xml in xmllist:
     tree = ET.parse(xml)
     print(xml + " parsed to tree")
     root = tree.getroot()
 
-    for node in root.findall("sentence_id"):
-        print("found sentence_id")
-        final_path = os.path.join(res_xml_path, xml)               
-        tree.write(final_path,encoding="UTF-8")
-        print(final_path)
+    for node in root.findall('.//Sentence'):
+        if node.attrib['sentence_id']:
+            print("node has attr")
+            print(node.attrib)
+            [y[1] for y in node.attrib]
+            if y in AnnoDict:
+                #sub = ET.SubElement(node, AnnoDict[node.attrib])
+                print(AnnoDict[node.attrib])
+            #tag = SubElement(node,'TagName')
+            #tag.attrib['attr'] = 'AttribValue'
+
+        #compare id to AnnoDict id's get related annotations
+        #add annotations to node
+        
+    final_path = os.path.join(res_xml_path, xml)               
+    tree.write(final_path,encoding="UTF-8")
+    print(final_path)
 xmlfile.close()
 print("xmlfile closed")
 
